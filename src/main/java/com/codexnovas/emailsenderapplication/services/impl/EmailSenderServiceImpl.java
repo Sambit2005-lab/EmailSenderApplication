e372d213167f1b4171615c8f37f3136d3959602a
Sambit2005-lab <143246577+Sambit2005-lab@users.noreply.github.com>
2024-08-22 10:59:32 +0530

    logic added to send the mail to single user

--- src/main/java/com/codexnovas/emailsenderapplication/services/impl/EmailSenderServiceImpl.java
+++ src/main/java/com/codexnovas/emailsenderapplication/services/impl/EmailSenderServiceImpl.java
@@ -1,6 +1,9 @@
 package com.codexnovas.emailsenderapplication.services.impl;
 
 import com.codexnovas.emailsenderapplication.services.EmailSenderService;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -8,10 +11,21 @@ import java.io.File;
 @Service
 public class EmailSenderServiceImpl implements EmailSenderService {
 
+    private JavaMailSender javaMailSender;  // we have to use this JavaMailSender yeh pahle se banaya hua hain springboot
+
+    public EmailSenderServiceImpl(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
 
     @Override
     public void sendEmailToSingleUser(String to, String subject, String msg) {
 
+        SimpleMailMessage simpleMailMessage=new SimpleMailMessage();
+        simpleMailMessage.setTo();
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(msg);
+        javaMailSender.send(simpleMailMessage);
 
     }
 
